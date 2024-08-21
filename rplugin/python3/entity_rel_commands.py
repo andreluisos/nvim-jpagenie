521f1ae0fc754a3cd86fdf558a867ea3798252a1
André Luís <andreluisos@msn.com>
2024-08-21 10:29:09 -0300

    Update EntityRelationshipCommands args

--- rplugin/python3/entity_rel_commands.py
+++ rplugin/python3/entity_rel_commands.py
@@ -16,32 +16,34 @@ class EntityRelationshipCommands(Base):
     @command("CreateManyToOneRelationship", nargs="*")
     def generate_id_entity_field(self, args: List[str]) -> None:
         # arg0 field_type (str)
-        # arg1 field_name (str)
-        # arg2 cascade_persist (bool)
-        # arg3 cascade_merge (bool)
-        # arg4 cascade_remove (bool)
-        # arg5 cascade_refresh (bool)
-        # arg6 cascade_detach (bool)
+        # arg1 cascade_persist (bool)
+        # arg2 cascade_merge (bool)
+        # arg3 cascade_remove (bool)
+        # arg4 cascade_refresh (bool)
+        # arg5 cascade_detach (bool)
+        # arg6 fetch_type (none | lazy | eager)
         # arg7 mapping_type (unidirectional_joincolumn | bidirectional_joincolumn)
         # arg8 nullable (bool)
+        # arg9 unique (bool)
         attach_debugger: bool = self.arg_validator.attach_debugger(args)
         if attach_debugger:
             self.logging.log(f"args:\n{args}", "debug")
         current_buffer: Buffer = self.nvim.current.buffer
         buffer_bytes = self.treesitter_lib.get_bytes_from_buffer(current_buffer)
         buffer_path = Path(self.nvim.current.buffer.name)
-        self.arg_validator.validate_args_length(args, 9)
+        self.arg_validator.validate_args_length(args, 11)
         validated_args = self.arg_validator.validate_args_type(
             args,
             [
                 "str",
-                "str",
                 "bool",
                 "bool",
                 "bool",
                 "bool",
                 "bool",
+                "fetch_type",
                 "mapping_type",
+                "bool",
                 "bool",
             ],
         )
