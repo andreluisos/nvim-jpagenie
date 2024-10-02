632a6c132a0cce85066d26136029f58c1c45331f
André Luís <andreluisos@msn.com>
2024-10-02 13:06:26 -0300

    Create FileReader for reading .lua ui files

---
+++ rplugin/python3/util/file_reader.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+
+class FileReader:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def read_ui_file_as_string(file_name: str) -> str:
+        file_path = (
+            Path(__file__).parent.resolve().parent.joinpath("ui").joinpath(file_name)
+        )
+        with open(file_path, "r") as f:
+            return f.read().strip()
