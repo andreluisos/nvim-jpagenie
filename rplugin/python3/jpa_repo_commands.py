cf134194beb70583a404cd6753e86a49e9431ec2
André Luís <andreluisos@msn.com>
2024-10-13 15:07:25 -0300

    Remove unnecessary code from JpaRepoCommands

--- rplugin/python3/jpa_repo_commands.py
+++ rplugin/python3/jpa_repo_commands.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import List, Optional
 
 from pynvim.api import Nvim
 from pynvim import command, plugin
@@ -12,16 +11,9 @@ class JpaRepoCommands(Base):
     def __init__(self, nvim: Nvim) -> None:
         super().__init__(nvim)
 
-    @command("CreateJPARepository", nargs="*")
-    def create_jpa_repo_repository(self, args: Optional[List[str]]) -> None:
+    @command("CreateJPARepository")
+    def create_jpa_repo_repository(self) -> None:
         # Optional arg: debug
-        attach_debugger: bool = False
-        if args:
-            attach_debugger: bool = self.arg_validator.attach_debugger(args)
-            if attach_debugger:
-                self.logging.log(f"args:\n{args}", "debug")
-            else:
-                self.arg_validator.validate_args_length(args, 0)
         root_path = self.path_lib.spring_project_root_path
         self.jpa_repo_lib.create_jpa_entity_for_current_buffer(
             Path(root_path), debug=True
