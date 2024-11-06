6692adb9d4e72bc2b189aeb789a2bc5718ca46db
André Luís <andreluisos@msn.com>
2024-11-06 19:47:47 -0300

    Refactor CommonUtils

--- rplugin/python3/utils/common_utils.py
+++ rplugin/python3/utils/common_utils.py
@@ -1,42 +1,30 @@
 from typing import List
-from pynvim.api import Nvim
+
+from tree_sitter import Tree
+from custom_types.java_file_data import JavaFileData
+from custom_types.declaration_type import DeclarationType
+from custom_types.log_level import LogLevel
 from utils.treesitter_utils import TreesitterUtils
+from utils.path_utils import PathUtils
 from pathlib import Path
 
-from re import sub
-from utils.path_utils import PathUtils
 from utils.logging import Logging
 
 
 class CommonUtils:
     def __init__(
         self,
-        nvim: Nvim,
         cwd: Path,
-        treesitter_utils: TreesitterUtils,
         path_utils: PathUtils,
+        treesitter_utils: TreesitterUtils,
         logging: Logging,
     ) -> None:
-        self.nvim = nvim
         self.cwd = cwd
-        self.treesitter_utils = treesitter_utils
         self.logging = logging
+        self.treesitter_utils = treesitter_utils
         self.path_utils = path_utils
 
-    def add_imports_to_buffer(
-        self, import_list: List[str], buffer_bytes: bytes, debug: bool
-    ) -> bytes:
-        updated_buffer_bytes = self.treesitter_utils.insert_import_paths_into_buffer(
-            buffer_bytes, import_list, debug
-        )
-        import_list = []
-        if debug:
-            self.logging.log(
-                f"Updated buffer: {updated_buffer_bytes.decode('utf-8')}", "debug"
-            )
-        return updated_buffer_bytes
-
-    def pluralize(self, word: str, debug: bool = False) -> str:
+    def pluralize_word(self, word: str, debug: bool = False) -> str:
         pluralized_word: str
         if word.endswith(("s", "sh", "ch", "x", "z")):
             pluralized_word = word + "es"
@@ -49,55 +37,129 @@ class CommonUtils:
         else:
             pluralized_word = word + "s"
         if debug:
-            self.logging.log(f"Pluralized word: {pluralized_word}", "debug")
+            self.logging.log(f"Pluralized word: {pluralized_word}", LogLevel.DEBUG)
         return pluralized_word
 
-    def generate_field_name(
-        self, field_type: str, plural: bool = False, debug: bool = False
+    def get_buffer_package_path(
+        self,
+        buffer_path: Path,
+        debug: bool = False,
     ) -> str:
-        field_name = field_type
-        if plural:
-            field_name = self.pluralize(field_name)
-        field_name = field_name[0].lower() + field_name[1:]
-        if debug:
-            self.logging.log(f"Field name: {field_name}", "debug")
-        return field_name
-
-    def generate_snaked_field_name(self, field_name: str, debug: bool = False) -> str:
-        snaked_field_name = sub(r"(?<!^)(?=[A-Z])", "_", field_name).lower()
-        if debug:
-            self.logging.log(
-                f"Snaked field name: {snaked_field_name}",
-                "debug",
-            )
-        return snaked_field_name
-
-    def merge_field_params(self, params: List[str], debug: bool = False) -> str:
-        merged_params = ", ".join(params)
-        if debug:
-            self.logging.log(f"Merged params: {merged_params}", "debug")
-        return ", ".join(params)
-
-    def generate_field_column_line(self, params: List[str], debug: bool = False) -> str:
-        merged_params = self.merge_field_params(params, debug)
-        column_line = f"@Column({merged_params})"
+        parent_path = buffer_path.parent
+        index_to_replace: int
+        try:
+            index_to_replace = parent_path.parts.index("main")
+        except ValueError:
+            error_msg = "Unable to split parent path"
+            self.logging.log(error_msg, LogLevel.DEBUG)
+            raise ValueError(error_msg)
+        package_path = str(Path(*parent_path.parts[index_to_replace + 2 :])).replace(
+            "/", "."
+        )
         if debug:
             self.logging.log(
                 [
-                    f"Merged params: {merged_params}",
-                    f"Column line: {column_line}",
+                    f"Index to replace: {index_to_replace}",
+                    f"Parent path: {str(parent_path)}",
+                    f"Package path: {package_path}",
                 ],
-                "debug",
+                LogLevel.DEBUG,
             )
-        return column_line
+        return package_path
 
-    def generate_field_body_line(
-        self, field_type: str, field_name: str, debug: bool = False
-    ) -> str:
-        field_body_line = f"private {field_type} {field_name};"
+    def get_all_java_files_data(self, debug: bool = False) -> List[JavaFileData]:
+        root_path = self.path_utils.get_spring_project_root_path()
+        files_found: List[JavaFileData] = []
+        decl_type_query_param = """
+        [
+            (class_declaration) 
+            (enum_declaration) 
+            (annotation_type_declaration) 
+            (interface_declaration) 
+            (record_declaration)
+        ] @decl_type 
+        """
+        for p in root_path.rglob("*.java"):
+            if "main" not in p.parts:
+                continue
+            buffer_tree = self.treesitter_utils.convert_buffer_to_tree(buffer=p)
+            query_result = self.treesitter_utils.query_match(
+                buffer_tree, decl_type_query_param
+            )
+            for result in query_result:
+                decl_name = result.child_by_field_name("name")
+                if decl_name and decl_name.text and result.text:
+                    decl_name_str = self.treesitter_utils.convert_bytes_to_string(
+                        decl_name.text
+                    )
+                    if decl_name_str == p.stem:
+                        result_tree = self.treesitter_utils.convert_buffer_to_tree(
+                            result.text
+                        )
+                        declaration_type: DeclarationType = DeclarationType.CLASS
+                        is_jpa_entity = (
+                            self.treesitter_utils.buffer_public_class_has_annotation(
+                                tree=result_tree, annotation_name="Entity", debug=debug
+                            )
+                        )
+                        if result.type == "class_declaration":
+                            declaration_type = DeclarationType.CLASS
+                        elif result.type == "enum_declaration":
+                            declaration_type = DeclarationType.ENUM
+                        elif result.type == "interface_declaration":
+                            declaration_type = DeclarationType.INTERFACE
+                        elif result.type == "annotation_declaration":
+                            declaration_type = DeclarationType.ANNOTATION
+                        else:
+                            declaration_type = DeclarationType.RECORD
+                        files_found.append(
+                            JavaFileData(
+                                file_name=decl_name_str,
+                                package_path=self.get_buffer_package_path(
+                                    buffer_path=p, debug=debug
+                                ),
+                                path=p,
+                                tree=buffer_tree,
+                                declaration_type=declaration_type,
+                                is_jpa_entity=is_jpa_entity,
+                            )
+                        )
         if debug:
             self.logging.log(
-                f"Field body line: {field_body_line }",
-                "debug",
+                [
+                    f"Root path: {str(root_path)}",
+                    f"Declaration type query param: {decl_type_query_param}",
+                    f"Files found:\n{[f.print() for f in files_found]}",
+                ],
+                LogLevel.DEBUG,
             )
-        return field_body_line
+        return files_found
+
+    def add_imports_to_file_tree(
+        self, import_list: List[str], file_tree: Tree, debug: bool = False
+    ) -> Tree:
+        package_query_param = "(package_declaration) @package_decl"
+        query_results = self.treesitter_utils.query_match(
+            tree=file_tree, query_param=package_query_param
+        )
+        if len(query_results) != 1:
+            error_msg = "File package not defined or defined incorrectly"
+            self.logging.log(error_msg, LogLevel.ERROR)
+            raise ValueError(error_msg)
+        insert_byte: int = query_results[0].end_byte + 1
+        merged_import_list = ";\n".join(import_list) + ";"
+        updated_tree = self.treesitter_utils.insert_code_at_position(
+            merged_import_list, insert_byte, file_tree
+        )
+        if debug:
+            self.logging.log(
+                [
+                    f"Package query param: {package_query_param}",
+                    f"Query results len: {len(query_results)}",
+                    f"Insert byte: {insert_byte}",
+                    f"Merged import list: {merged_import_list}",
+                    f"Updated tree: {updated_tree}",
+                ],
+                LogLevel.DEBUG,
+            )
+        return updated_tree
